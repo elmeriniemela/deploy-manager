d03325ac58aa956f69559417a8fbc38524428fc0
Elmeri Niemelä <niemela.elmeri@gmail.com>
2023-11-26 15:19:06 +0200

    autocommit

--- agentd/api.py
+++ agentd/api.py
@@ -20,7 +20,8 @@ def new_instance(name, uid, http_port, gevent_port):
         (sql.SQL("CREATE DATABASE {uid} WITH OWNER={uid}").format(uid=sql.Identifier(uid)),),
         (sql.SQL("REVOKE ALL ON DATABASE {uid} FROM public").format(uid=sql.Identifier(uid)),),
     ]
-    with psycopg2.connect(dbname='postgres', autocommit=True) as conn:
+    with psycopg2.connect(dbname='postgres') as conn:
+        conn.set_session(autocommit=True) # CREATE DATABASE cannot be run inside a transaction block.
         with conn.cursor() as cur:
             for args in queries:
                 cur.execute(*args)
