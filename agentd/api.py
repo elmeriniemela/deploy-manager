2196e9a0cba1dd822d20e31ce36bf543c01ff788
Elmeri Niemelä <niemela.elmeri@gmail.com>
2023-11-26 15:28:03 +0200

    dir

--- agentd/api.py
+++ agentd/api.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import psycopg2
 from psycopg2 import sql
 import secrets
@@ -38,6 +39,7 @@ def new_instance(name, uid, http_port, gevent_port):
         template = Template(fp.read())
 
     conf = template.render(uid=uid, pw=pw)
+    os.makedirs(f'/etc/odoo/{uid}', mode=0o700, exist_ok=True)
     with open(f'/etc/odoo/{uid}/odoo.conf', 'w') as fp:
         fp.write(conf)
 
