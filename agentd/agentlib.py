4a50aa45e0a6f2c2946e0da999f9a9eab46aff45
Elmeri Niemelä <niemela.elmeri@gmail.com>
2023-11-26 16:41:42 +0200

    validation

--- agentd/agentlib.py
+++ agentd/agentlib.py
@@ -4,7 +4,7 @@ _logger = logging.getLogger(__name__)
 
 def register(func):
     def wraps(*args): #XML-RPC doesn't have a concept of 'keyword arguments'
-        _logger.info(f"Calling {func.__name__}, {args=}.")
+        _logger.info(f"Call {func.__name__}({args})")
         return func(*args)
     wraps._rpc = True
     wraps.__name__ = func.__name__
--- agentd/api.py
+++ agentd/api.py
@@ -4,17 +4,45 @@ import psycopg2
 from psycopg2 import sql
 import secrets
 import subprocess
+import re
 from jinja2 import Template
 from agentlib import register
 _logger = logging.getLogger(__name__)
+
+
+def _is_valid_hostname(hostname):
+    # https://stackoverflow.com/a/33214423
+    if hostname[-1] == ".":
+        # strip exactly one dot from the right, if present
+        hostname = hostname[:-1]
+    if len(hostname) > 253:
+        return False
+
+    labels = hostname.split(".")
+
+    # the TLD must be not all-numeric
+    if re.match(r"[0-9]+$", labels[-1]):
+        return False
+
+    allowed = re.compile(r"(?!-)[a-z0-9-]{1,63}(?<!-)$")
+    return all(allowed.match(label) for label in labels)
 
 @register
 def test():
     print("test")
 
-
 @register
 def new_instance(name, uid, http_port, gevent_port):
+    assert isinstance(name, str)
+    assert isinstance(uid, str)
+    assert isinstance(http_port, int)
+    assert isinstance(gevent_port, int)
+    assert _is_valid_hostname(name)
+    try:
+        int(uid, 16)
+    except ValueError:
+        raise ValueError("Invalid UID, expected a hexadecimal number.")
+
     pw = secrets.token_hex(32)
     queries = [
         (sql.SQL("CREATE ROLE {uid} NOSUPERUSER NOCREATEDB NOCREATEROLE NOINHERIT LOGIN ENCRYPTED PASSWORD %s").format(uid=sql.Identifier(uid)), (pw,)),
