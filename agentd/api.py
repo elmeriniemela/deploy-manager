2d69400af1d4bff141eeb39bcf422dd353731db6
Elmeri Niemelä <niemela.elmeri@gmail.com>
2023-11-26 16:09:43 +0200

    error handling

--- agentd/api.py
+++ agentd/api.py
@@ -43,7 +43,7 @@ def new_instance(name, uid, http_port, gevent_port):
     with open(f'/etc/odoo/{uid}/odoo.conf', 'w') as fp:
         fp.write(conf)
 
-    subprocess.run(
+    commands = [
         [
             'docker', 'run',
             '-v', f'/opt/odoo-agent/src:/mnt:ro',
@@ -57,29 +57,24 @@ def new_instance(name, uid, http_port, gevent_port):
             '--name', uid,
             '-t', '-d', 'odoo-src:16.0',
         ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        check=True,
-        encoding='utf-8',
-    )
-
-    subprocess.run(
         [
             'docker', 'exec', '-it', uid, 'odoo',
             '--init=base',
             '--http-port=9999',
             '-stop-after-init',
         ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        check=True,
-        encoding='utf-8',
-    )
-
-    subprocess.run(
         ['docker', 'restart', uid],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        check=True,
-        encoding='utf-8',
-    )
+    ]
+
+    for cmd in commands:
+        try:
+            subprocess.run(
+                cmd,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                check=True,
+                encoding='utf-8',
+            )
+        except subprocess.CalledProcessError as e:
+            raise Exception(e.output)
+
