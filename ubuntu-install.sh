bb0a45d2da621c307e06f18f45acdecb24094d88
Elmeri Niemelä <niemela.elmeri@gmail.com>
2024-02-18 13:17:14 +0200

    fixes

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -26,4 +26,7 @@ cp systemd/system/odoo-agent.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable odoo-agent.service --now
 
+rsync -avz postgres/ /etc/postgresql/*/main/
+rsync -avz nginx/ /etc/nginx/
+
 exit 1
