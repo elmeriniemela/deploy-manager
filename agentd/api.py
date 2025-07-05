d51d7dd78b3de08efd2faea0040d2ec41a86434a
Elmeri Niemelä <niemela.elmeri@gmail.com>
2025-07-05 17:44:18 +0300

    fix restart notifications

--- agentd/api.py
+++ agentd/api.py
@@ -297,6 +297,13 @@ def self_upgrade(uid, callback_url):
         )
         _logger.info(resp.text)
         restart(uid)
+        resp = requests.post(
+            url=callback_url,
+            json={
+                'method': 'restart',
+                'uid': uid,
+            }
+        )
 
     threading.Thread(target=thread_worker).start()
 
