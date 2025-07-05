aedcfc2b934113b68ded42c5ea0d39508be3edb4
Elmeri Niemelä <niemela.elmeri@gmail.com>
2025-07-05 17:52:06 +0300

    imp restart call

--- agentd/api.py
+++ agentd/api.py
@@ -294,16 +294,20 @@ def self_upgrade(uid, callback_url):
                 'uid': uid,
                 'logs': logs,
             }
+            timeout=15,
         )
         _logger.info(resp.text)
         restart(uid)
+        time.sleep(1)
         resp = requests.post(
             url=callback_url,
             json={
                 'method': 'restart',
                 'uid': uid,
-            }
+            },
+            timeout=15,
         )
+        _logger.info(resp.text)
 
     threading.Thread(target=thread_worker).start()
 
